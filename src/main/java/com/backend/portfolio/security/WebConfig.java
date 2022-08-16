bcfc337fdaf815caa9d84b8924d1b5ef04f66571
Juan <juan.carosella@gmail.com>
2022-08-16 12:54:56 -0300

    Update WebConfig.java

--- src/main/java/com/backend/portfolio/security/WebConfig.java
+++ src/main/java/com/backend/portfolio/security/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://181.231.74.169:4200")
+                .allowedOrigins("https://portfolio-front-353603.web.app/")
                 .allowCredentials(true)
                 .allowedHeaders("*")
                 .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
