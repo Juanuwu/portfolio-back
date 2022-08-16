bf8eef50b0063b77a7b241643039f6ae8804d0ab
Juan <juan.carosella@gmail.com>
2022-08-16 15:24:04 -0300

    cookies

--- src/main/java/com/backend/portfolio/controllers/UsuarioController.java
+++ src/main/java/com/backend/portfolio/controllers/UsuarioController.java
@@ -80,8 +80,8 @@ public class UsuarioController {
                             .sign(algorithm);
                     final ResponseCookie access = ResponseCookie
                             .from("access_token", access_token)
-                            .secure(false)
-                            .httpOnly(false)
+                            .secure(true)
+                            .httpOnly(true)
                             .path("/")
                             .sameSite("none")
                             .build();
